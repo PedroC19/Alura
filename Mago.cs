ba1cbd7bd5e0bc0f310a2d380046f82b9a1547e7
PedroC <117571983+PedroC19@users.noreply.github.com>
2026-03-30 20:02:52 -0300

    Arqueiro e Mago

---
+++ Mago.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Random;
+using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
+
+namespace RPG.Personagem
+{
+    public class Mago : Personagem
+    {
+        public Mago(string nome) : base(nome, 10, 5, 5)
+        {
+            
+        }
+        public override Atacar(Personagem alvo)
+        {
+                double dano = Ataque - alvo.Defesa;
+                    if(dano < 0 ) dano = 0;
+                alvo.ReceberDano(dano);
+        }
+        public void usarEspecial()
+        {
+            
+        }
+    }
+}
